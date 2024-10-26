12a6adcce880810d1c61f8b9544b5a8b38fdf3be
RAFAELESPADA <pidgeypidgey1@gmail.com>
2024-10-26 19:49:47 -0300

    FIXED COOLDOWN BAR

--- eclipse-workspace/WaveCore/src/main/java/net/wavemc/core/bukkit/api/ActionBar.java
+++ eclipse-workspace/WaveCore/src/main/java/net/wavemc/core/bukkit/api/ActionBar.java
@@ -1,5 +1,7 @@
 package net.wavemc.core.bukkit.api;
 
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
@@ -11,16 +13,10 @@ import net.minecraft.server.v1_8_R3.PacketPlayOutChat;
 public class ActionBar {
 
     public static void sendActionBar(Player player, String rawMessage) {
-        CraftPlayer craftPlayer = (CraftPlayer) player;
-        IChatBaseComponent cbc = ChatSerializer.a("{\"text\": \"" + rawMessage + "\"}");
-        PacketPlayOutChat packetPlayOutChat = new PacketPlayOutChat(cbc, (byte) 2);
-        craftPlayer.getHandle().playerConnection.sendPacket(packetPlayOutChat);
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(rawMessage.replace("&", "§")));
     }
 
     public static void sendActionBarWithoutCheck(Player player, String rawMessage) {
-        CraftPlayer craftPlayer = (CraftPlayer) player;
-        IChatBaseComponent cbc = ChatSerializer.a("{\"text\": \"" + rawMessage + "\"}");
-        PacketPlayOutChat packetPlayOutChat = new PacketPlayOutChat(cbc, (byte) 2);
-        craftPlayer.getHandle().playerConnection.sendPacket(packetPlayOutChat);
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(rawMessage.replace("&", "§")));
     }
 }
